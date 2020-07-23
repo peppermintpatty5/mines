863d78301fc28e8b39eb85204b8558ac44234f1e
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2020-07-22 23:05:23 -0400

    Add files via upload

---
+++ mines.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+from itertools import product
+from random import sample
+
+
+def adjacent(*t):
+    return set(product(*((n - 1, n, n + 1) for n in t))) - {tuple(t)}
+
+
+class Minesweeper:
+    """Single-use game of minesweeper"""
+
+    def __init__(self, rows, cols, nMines, lives=1):
+        self.rows = int(rows)
+        self.cols = int(cols)
+        self.nMines = int(nMines)
+        self.lives = int(lives)
+        self.dug = set()
+        self.flags = set()
+        self.mines = set()
+
+    def __getitem__(self, index):
+        r, c = index
+        reveal = len(self.dug & self.mines) >= self.lives
+        return (
+            "@"
+            if (r, c) in self.dug and (r, c) in self.mines
+            else "^"
+            if (r, c) in self.flags and (r, c) in self.mines
+            else "^X"[reveal]
+            if (r, c) in self.flags
+            else "-*"[reveal]
+            if (r, c) in self.mines
+            else str(len(adjacent(r, c) & self.mines))
+            if (r, c) in self.dug
+            else "-"
+        )
+
+    def __str__(self):
+        return "\n".join(
+            " ".join(self[(r, c)] for c in range(self.cols)) for r in range(self.rows)
+        ).replace("0", " ")
+
+    def dig(self, r, c):
+        if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.flags:
+            if self.dug == set():  # Generate the minefield on first click
+                self.mines = set(
+                    sample(
+                        set(product(range(self.rows), range(self.cols))) - {(r, c)},
+                        self.nMines,
+                    )
+                )
+
+            s = {(r, c)}
+            while len(s) > 0:
+                r, c = s.pop()
+                if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.dug:
+                    adj = adjacent(r, c)
+                    f, m = len(adj & self.flags), len(adj & self.mines)
+                    if (r, c) not in self.mines and (f == m or m == 0):
+                        s.update(adj - self.flags)
+                    self.dug.add((r, c))
+
+    def flag(self, r, c):
+        if 0 <= r < self.rows and 0 <= c < self.cols:
+            if (r, c) in self.flags:
+                self.flags.remove((r, c))
+            elif (r, c) not in self.dug:
+                self.flags.add((r, c))
+
+
+m = Minesweeper(16, 16, 40)
+while True:
+    try:
+        opt = input("Enter 'd' to dig or 'f' to flag: ").lower()
+        if opt == "d" or opt == "f":
+            r = int(input("Enter row: "))
+            c = int(input("Enter col: "))
+            (m.flag if opt == "f" else m.dig)(r, c)
+            print(m)
+    except ValueError as e:
+        pass
+    except EOFError as e:
+        print("Exiting...")
+        break
