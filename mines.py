9570a1e55fc92e08155ce3ef9722ff4c84708349
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2020-07-24 21:11:17 -0400

    General code cleanup in mines.py
    Added param to disable automatic chording of '0' cells
    Testing for empty set is faster via cardinality

--- mines.py
+++ mines.py
@@ -20,76 +20,81 @@ class Minesweeper:
         self.flags = set()
         self.mines = set()
         if self.nMines > self.rows * self.cols - 1:
-            raise ValueError("Too many mines for grid size")
-
-    def __getitem__(self, index):
-        r, c = index
-        reveal = len(self.dug & self.mines) >= self.lives
-        return (
-            "@"
-            if (r, c) in self.dug and (r, c) in self.mines
-            else "^"
-            if (r, c) in self.flags and (r, c) in self.mines
-            else "^X"[reveal]
-            if (r, c) in self.flags
-            else "-*"[reveal]
-            if (r, c) in self.mines
-            else str(len(adjacent(r, c) & self.mines))
-            if (r, c) in self.dug
-            else "-"
-        )
+            raise ValueError("too many mines for grid size")
 
     def __str__(self):
+        lose = len(self.dug & self.mines) >= self.lives
         return "\n".join(
-            " ".join(self[(r, c)] for c in range(self.cols)) for r in range(self.rows)
-        ).replace("0", " ")
+            " ".join(
+                "@"
+                if (r, c) in self.dug and (r, c) in self.mines
+                else "^"
+                if (r, c) in self.flags and (r, c) in self.mines
+                else ("X" if lose else "^")
+                if (r, c) in self.flags
+                else ("*" if lose else "-")
+                if (r, c) in self.mines
+                else str(len(adjacent(r, c) & self.mines))
+                if (r, c) in self.dug
+                else "-"
+                for c in range(self.cols)
+            )
+            for r in range(self.rows)
+        )
 
-    def dig(self, r, c):
-        if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.flags:
-            if self.dug == set():  # Generate the minefield on first click
-                self.mines = set(
-                    sample(
-                        set(product(range(self.rows), range(self.cols))) - {(r, c)},
-                        self.nMines,
-                    )
+    def dig(self, r, c, auto=True):
+        if not (0 <= r < self.rows and 0 <= c < self.cols):
+            raise IndexError("grid index out of range")
+
+        if len(self.mines) != self.nMines:  # Generate minefield
+            self.mines = set(
+                sample(
+                    set(product(range(self.rows), range(self.cols))) - {(r, c)},
+                    self.nMines,
                 )
+            )
 
-            s = set()
-            if (r, c) in self.dug:  # Uncover adjacent cells (chord)
+        if (r, c) in self.dug:  # Uncover adjacent cells (chord)
+            a = adjacent(r, c)
+            s = a - self.flags if len(a & self.flags) == len(a & self.mines) else set()
+        else:
+            s = {(r, c)} if (r, c) not in self.flags else set()
+
+        while len(s) != 0:
+            r, c = s.pop()
+            if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.dug:
                 a = adjacent(r, c)
-                if len(a & self.flags) == len(a & self.mines):
-                    s = a - self.flags
-            else:
-                s = {(r, c)}
-
-            while len(s) > 0:
-                r, c = s.pop()
-                if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.dug:
-                    adj = adjacent(r, c)
-                    f, m = len(adj & self.flags), len(adj & self.mines)
-                    if (r, c) not in self.mines and (f == m or m == 0):
-                        s.update(adj - self.flags)
-                    self.dug.add((r, c))
+                if auto and (r, c) not in self.mines and len(a & self.mines) == 0:
+                    s.update(a - self.flags)
+                self.dug.add((r, c))
 
     def flag(self, r, c):
-        if 0 <= r < self.rows and 0 <= c < self.cols:
-            if (r, c) in self.flags:
-                self.flags.remove((r, c))
-            elif (r, c) not in self.dug:
-                self.flags.add((r, c))
+        if not (0 <= r < self.rows and 0 <= c < self.cols):
+            raise IndexError("grid index out of range")
+
+        if (r, c) in self.flags:
+            self.flags.remove((r, c))
+        elif (r, c) not in self.dug:
+            self.flags.add((r, c))
 
 
 if __name__ == "__main__":
     m = Minesweeper(16, 30, 99)
     while True:
         try:
-            opt = input("Enter 'd' to dig or 'f' to flag: ").lower()
-            if opt == "d" or opt == "f":
+            opt = input("Enter 'd' to dig or 'f' to flag: ")
+            if opt.lower() == "d" or opt.lower() == "f":
                 r = int(input("Enter row: "))
                 c = int(input("Enter col: "))
-                (m.flag if opt == "f" else m.dig)(r, c)
-                print(m)
-        except ValueError as e:
+                m.dig(r, c, auto=True) if opt.lower() == "d" else m.flag(r, c)
+                print(str(m).replace("0", " "))
+            else:
+                raise ValueError(f"invalid option '{opt}'")
+        except (ValueError, IndexError) as e:
+            print(e)
+            pass
+        except KeyboardInterrupt as e:
+            print("")
             pass
         except EOFError as e:
             print("Exiting...")
