e0e11ee6235598821f00955870a15204ed8bbc1f
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2020-07-29 23:08:59 -0400

    Everything is a bit messy right now...

--- README.md
+++ README.md
@@ -4,16 +4,20 @@ Set-based implementation of minesweeper in Python
 
 ## Overview
 
-Typically, minesweeper is implemented using a 2D array of cells. The following table shows an example grid of all possible cells. The two missing combinations reflect the fact that `dug` and `flag` properties are mutually exclusive.
+Typically, minesweeper is implemented using a 2D array of cells. The following table shows an example grid containing all possible cells. The two missing combinations reflect the fact that `dug` and `flag` properties are mutually exclusive (cannot both be true).
 
-| row  | col  |  dug  | flag  | mine  |
-| :--- | :--- | :---: | :---: | :---: |
-| 0    | 0    |       |       |       |
-| 0    | 1    |       |       |   X   |
-| 0    | 2    |       |   X   |       |
-| 1    | 0    |       |   X   |   X   |
-| 1    | 1    |   X   |       |       |
-| 1    | 2    |   X   |       |   X   |
+```txt
++-----+-----+------+------+------+
+| row | col | dug  | flag | mine |
++-----+-----+------+------+------+
+|   0 |   0 |    0 |    0 |    0 |
+|   0 |   1 |    0 |    0 |    1 |
+|   0 |   2 |    0 |    1 |    0 |
+|   1 |   0 |    0 |    1 |    1 |
+|   1 |   1 |    1 |    0 |    0 |
+|   1 |   2 |    1 |    0 |    1 |
++-----+-----+------+------+------+
+```
 
 The printout of the grid would look like this:
 
@@ -22,7 +26,7 @@ The printout of the grid would look like this:
 ^ 3 @
 ```
 
-Now, the above table translated into three sets:
+Now, the above table translated into three sets. Like before, the `dug` and `flag` sets are disjoint (intersection is the empty set).
 
 ```py
 dug = {(1, 1), (1, 2)}
@@ -30,10 +34,36 @@ flag = {(0, 2), (1, 0)}
 mine = {(0, 1), (1, 0), (1, 2)}
 ```
 
-The end result of this simple data transformation is a program that is less efficient, but far more elegant in terms of mathematical reasoning and code style. It also retains the same time/space complexities, as will be explained below.
+The result of this alteration is a program that is less efficient, but far more elegant in terms of mathematical reasoning and code style. It also retains the same time/space complexities, as will be explained below.
+
+## Mathematical background
+
+The following set properties translate directly into Python code optimizations:
+<!-- One day, GitHub will bestow upon us built-in LaTeX rendering. -->
+![x \in A \cup B \equiv x \in A \vee x \in B](https://latex.codecogs.com/svg.latex?x%20%5Cin%20A%20%5Ccup%20B%20%5Cequiv%20x%20%5Cin%20A%20%5Cvee%20x%20%5Cin%20B)
+
+![x \in A \cap B \equiv x \in A \wedge x \in B](https://latex.codecogs.com/svg.latex?x%20%5Cin%20A%20%5Ccap%20B%20%5Cequiv%20x%20%5Cin%20A%20%5Cwedge%20x%20%5Cin%20B)
+
+![x \in A - B \equiv x \in A \wedge x \notin B](https://latex.codecogs.com/svg.latex?x%20%5Cin%20A%20-%20B%20%5Cequiv%20x%20%5Cin%20A%20%5Cwedge%20x%20%5Cnotin%20B)
+
+```py
+(x in A | B) is (x in A or x in B)
+(x in A & B) is (x in A and x in B)
+(x in A - B) is (x in A and x not in B)
+```
+
+In cases where `A` is of constant size, `&` and `-` are `O(1)`.
+
+| Operation |  Average case  |
+| :-------- | :------------: |
+| `A \| B`  |   `O(a + b)`   |
+| `A & B`   | `O(min(a, b))` |
+| `A - B`   |     `O(a)`     |
+
+## Algorithm descriptions
 
 *Note:* For simplicity, the number of mines is assumed to be proportional to the number of cells. Therefore, `n` will be used to describe both quantities simultaneously.
 
-----
-
-WIP
+* **dig** `O(1)` - Uncovers a cell by adding it to `dug`
+* **flag** `O(1)` - Toggles a cell's flag on/off by adding/removing it to/from `dug`
+* **chord** `O(1)` - Checks if the number of adjacent flags equals the number of adjacent mines, then adds the adjacent non-flag cells to `dug`
--- mines.py
+++ mines.py
@@ -2,94 +2,142 @@
 
 from itertools import product
 from random import sample
-
-
-def adjacent(*t):
-    return set(product(*((n - 1, n, n + 1) for n in t))) - {tuple(t)}
+from time import clock
+from sys import argv
 
 
 class Minesweeper:
     """Single-use game of minesweeper"""
 
-    def __init__(self, rows, cols, nMines, lives=1):
-        self.rows = abs(int(rows))
-        self.cols = abs(int(cols))
-        self.nMines = abs(int(nMines))
-        self.lives = abs(int(lives))
-        self.dug = set()
-        self.flags = set()
-        self.mines = set()
-        if self.nMines > self.rows * self.cols - 1:
-            raise ValueError("too many mines for grid size")
+    def __init__(self, rows, cols, mines, lives=1):
+        self.rows = rows
+        self.cols = cols
+        self.mines = mines
+        self.lives = lives
+        self.__r = set()
+        self.__f = set()
+        self.__m = set()
 
     def __str__(self):
-        lose = len(self.dug & self.mines) >= self.lives
+        lose = self.lose()
         return "\n".join(
             " ".join(
                 "@"
-                if (r, c) in self.dug and (r, c) in self.mines
+                if (r, c) in self.__r and (r, c) in self.__m
                 else "^"
-                if (r, c) in self.flags and (r, c) in self.mines
+                if (r, c) in self.__f and (r, c) in self.__m
                 else ("X" if lose else "^")
-                if (r, c) in self.flags
+                if (r, c) in self.__f
                 else ("*" if lose else "-")
-                if (r, c) in self.mines
-                else str(len(adjacent(r, c) & self.mines))
-                if (r, c) in self.dug
+                if (r, c) in self.__m
+                else str(len(Minesweeper.__adjacent(r, c) & self.__m))
+                if (r, c) in self.__r
                 else "-"
                 for c in range(self.cols)
             )
             for r in range(self.rows)
         )
 
-    def dig(self, r, c, auto=True):
-        if not (0 <= r < self.rows and 0 <= c < self.cols):
-            raise IndexError("grid index out of range")
+    @staticmethod
+    def __adjacent(*t):
+        return set(product(*((n - 1, n, n + 1) for n in t))) - {tuple(t)}
 
-        if len(self.mines) != self.nMines:  # Generate minefield
-            self.mines = set(
-                sample(
-                    set(product(range(self.rows), range(self.cols))) - {(r, c)},
-                    self.nMines,
-                )
+    def __generate(self, r, c):
+        self.__m = set(
+            sample(
+                set(product(range(self.rows), range(self.cols))) - {(r, c)}, self.mines,
             )
+        )
 
-        if (r, c) in self.dug:  # Uncover adjacent cells (chord)
-            a = adjacent(r, c)
-            s = a - self.flags if len(a & self.flags) == len(a & self.mines) else set()
+    def lose(self):
+        return len(self.__r & self.__m) >= self.lives
+
+    def win(self):
+        return (
+            not self.lose()
+            and len(self.__r - self.__m) == self.rows * self.cols - self.mines
+        )
+
+    def primaryClick(self, r, c, auto=True):
+        if len(self.__m) != self.mines:
+            self.__generate(r, c)
+
+        a = Minesweeper.__adjacent(r, c)
+        if (r, c) not in self.__r:
+            s = {(r, c)} if (r, c) not in self.__f else set()
         else:
-            s = {(r, c)} if (r, c) not in self.flags else set()
+            s = a - self.__f if len(a & self.__f) == len(a & self.__m) else set()
 
         while len(s) != 0:
             r, c = s.pop()
-            if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.dug:
-                a = adjacent(r, c)
-                if auto and (r, c) not in self.mines and len(a & self.mines) == 0:
-                    s.update(a - self.flags)
-                self.dug.add((r, c))
+            if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.__r:
+                a = Minesweeper.__adjacent(r, c)
+                if auto and (r, c) not in self.__m and len(a & self.__m) == 0:
+                    s.update(a - self.__f)
+                self.__r.add((r, c))
 
-    def flag(self, r, c):
-        if not (0 <= r < self.rows and 0 <= c < self.cols):
-            raise IndexError("grid index out of range")
-
-        if (r, c) in self.flags:
-            self.flags.remove((r, c))
-        elif (r, c) not in self.dug:
-            self.flags.add((r, c))
+    def secondaryClick(self, r, c):
+        if (r, c) in self.__f:
+            self.__f.remove((r, c))
+        elif (r, c) not in self.__r:
+            self.__f.add((r, c))
 
 
 if __name__ == "__main__":
-    m = Minesweeper(16, 30, 99)
+    # This section of code is hideous, might fix it later
+
+    kwargs, keys = {}, ["rows", "cols", "mines", "lives"]
+    for a in argv[1:]:
+        try:
+            for i, t in enumerate(keys):
+                if a.startswith(f"-{t}="):
+                    kwargs[t] = int(a.split("=")[1])
+                    break
+                elif i == len(keys) - 1:
+                    raise TypeError()
+        except:
+            print(f"Invalid argument '{a}'")
+            exit()
+    for k, v in kwargs.items():
+        if v < 0:
+            print(f"'{k}' cannot be negative")
+            exit()
+    try:
+        if kwargs["mines"] > kwargs["rows"] * kwargs["cols"] - 1:
+            print("Too many mines")
+            exit()
+    except Exception as e:
+        print(f"Missing argument for {e}")
+        exit()
+
+    mode = 1  # click mode (primary/secondary)
+    m = Minesweeper(**kwargs)
     while True:
         try:
-            opt = input("Enter 'd' to dig or 'f' to flag: ")
-            if opt.lower() == "d" or opt.lower() == "f":
-                r = int(input("Enter row: "))
-                c = int(input("Enter col: "))
-                m.dig(r, c, auto=True) if opt.lower() == "d" else m.flag(r, c)
-                print(str(m).replace("0", " "))
+            r = input(f"row{'>' * mode} ")
+            if r == "":
+                mode = 1 if mode == 2 else 2
+                continue
             else:
-                raise ValueError(f"invalid option '{opt}'")
+                r = int(r)
+            c = input(f"col{'>' * mode} ")
+            if c == "":
+                mode = 1 if mode == 2 else 2
+                continue
+            else:
+                c = int(c)
+
+            if 0 <= r < m.rows and 0 <= c < m.cols:
+                m.primaryClick(r, c) if mode == 1 else m.secondaryClick(r, c)
+                print(str(m).replace("0", " "))
+                if m.lose():
+                    print("YOU LOSE!")
+                    exit()
+                if m.win():
+                    print("YOU WIN!")
+                    exit()
+            else:
+                raise IndexError("Invalid index")
         except (ValueError, IndexError) as e:
             print(e)
             pass
