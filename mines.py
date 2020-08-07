7c274a026f74a22722a4acb922b8020530565159
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2020-08-06 21:39:36 -0400

    Removed lousy demo code (for now), tried to made code more readable

--- mines.py
+++ mines.py
@@ -2,148 +2,109 @@
 
 from itertools import product
 from random import sample
-from time import clock
-from sys import argv
 
 
-class Minesweeper:
+class Grid:
     """Single-use game of minesweeper"""
 
     def __init__(self, rows, cols, mines, lives=1):
-        self.rows = rows
-        self.cols = cols
-        self.mines = mines
-        self.lives = lives
-        self.__r = set()
-        self.__f = set()
-        self.__m = set()
+        if mines > rows * cols - 1:
+            raise ValueError("too many mines")
+        self.__rows = rows
+        self.__cols = cols
+        self.__numMines = mines
+        self.__numLives = lives
+        self.__revealed = set()
+        self.__flags = set()
+        self.__mines = set()
+
+    def __contains__(self, item):
+        try:
+            r, c = item
+            return (
+                int(r) == r
+                and int(c) == c
+                and 0 <= r < self.__rows
+                and 0 <= c < self.__cols
+            )
+        except Exception as e:
+            return False
+
+    def __repr__(self):
+        return ",\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
 
     def __str__(self):
+        R, F, M = self.__revealed, self.__flags, self.__mines
         lose = self.lose()
         return "\n".join(
             " ".join(
                 "@"
-                if (r, c) in self.__r and (r, c) in self.__m
+                if (r, c) in R and (r, c) in M
                 else "^"
-                if (r, c) in self.__f and (r, c) in self.__m
+                if (r, c) in F and (r, c) in M
                 else ("X" if lose else "^")
-                if (r, c) in self.__f
+                if (r, c) in F
                 else ("*" if lose else "-")
-                if (r, c) in self.__m
-                else str(len(Minesweeper.__adjacent(r, c) & self.__m))
-                if (r, c) in self.__r
+                if (r, c) in M
+                else str(len(self.__adjacent(r, c) & M))
+                if (r, c) in R
                 else "-"
-                for c in range(self.cols)
+                for c in range(self.__cols)
             )
-            for r in range(self.rows)
+            for r in range(self.__rows)
         )
 
-    @staticmethod
-    def __adjacent(*t):
-        return set(product(*((n - 1, n, n + 1) for n in t))) - {tuple(t)}
+    def __adjacent(self, r, c):
+        return {
+            x
+            for x in product({r - 1, r, r + 1}, {c - 1, c, c + 1})
+            if x in self and x != (r, c)
+        }
 
     def __generate(self, r, c):
-        self.__m = set(
+        self.__mines |= set(
             sample(
-                set(product(range(self.rows), range(self.cols))) - {(r, c)}, self.mines,
+                set(product(range(self.__rows), range(self.__cols))) - {(r, c)},
+                self.__numMines,
             )
         )
 
     def lose(self):
-        return len(self.__r & self.__m) >= self.lives
+        return len(self.__revealed & self.__mines) >= self.__numLives
 
     def win(self):
+        R, F, M = self.__revealed, self.__flags, self.__mines
         return (
             not self.lose()
-            and len(self.__r - self.__m) == self.rows * self.cols - self.mines
+            and len(R - M) == self.__rows * self.__cols - self.__numMines
         )
 
-    def primaryClick(self, r, c, auto=True):
-        if len(self.__m) != self.mines:
-            self.__generate(r, c)
+    def leftClick(self, r, c, auto=True):
+        x = (r, c)
+        R, F, M = self.__revealed, self.__flags, self.__mines
+        if x not in self:
+            raise ValueError("invalid row/col")
 
-        a = Minesweeper.__adjacent(r, c)
-        if (r, c) not in self.__r:
-            s = {(r, c)} if (r, c) not in self.__f else set()
-        else:
-            s = a - self.__f if len(a & self.__f) == len(a & self.__m) else set()
+        if len(M) != self.__numMines:
+            self.__generate(*x)
 
-        while len(s) != 0:
-            r, c = s.pop()
-            if 0 <= r < self.rows and 0 <= c < self.cols and (r, c) not in self.__r:
-                a = Minesweeper.__adjacent(r, c)
-                if auto and (r, c) not in self.__m and len(a & self.__m) == 0:
-                    s.update(a - self.__f)
-                self.__r.add((r, c))
+        A = self.__adjacent(*x)
+        S = ({x} if x not in R else A if len(A & F) == len(A & M) else set()) - R - F
 
-    def secondaryClick(self, r, c):
-        if (r, c) in self.__f:
-            self.__f.remove((r, c))
-        elif (r, c) not in self.__r:
-            self.__f.add((r, c))
+        while len(S) != 0:
+            x = S.pop()
+            A = self.__adjacent(*x)
+            if auto and x not in M and len(A & M) == 0:
+                S |= A - R - F
+            R |= {x}
 
+    def rightClick(self, r, c):
+        x = (r, c)
+        R, F, M = self.__revealed, self.__flags, self.__mines
+        if x not in self:
+            raise ValueError("invalid row/col")
 
-if __name__ == "__main__":
-    # This section of code is hideous, might fix it later
-
-    kwargs, keys = {}, ["rows", "cols", "mines", "lives"]
-    for a in argv[1:]:
-        try:
-            for i, t in enumerate(keys):
-                if a.startswith(f"-{t}="):
-                    kwargs[t] = int(a.split("=")[1])
-                    break
-                elif i == len(keys) - 1:
-                    raise TypeError()
-        except:
-            print(f"Invalid argument '{a}'")
-            exit()
-    for k, v in kwargs.items():
-        if v < 0:
-            print(f"'{k}' cannot be negative")
-            exit()
-    try:
-        if kwargs["mines"] > kwargs["rows"] * kwargs["cols"] - 1:
-            print("Too many mines")
-            exit()
-    except Exception as e:
-        print(f"Missing argument for {e}")
-        exit()
-
-    mode = 1  # click mode (primary/secondary)
-    m = Minesweeper(**kwargs)
-    while True:
-        try:
-            r = input(f"row{'>' * mode} ")
-            if r == "":
-                mode = 1 if mode == 2 else 2
-                continue
-            else:
-                r = int(r)
-            c = input(f"col{'>' * mode} ")
-            if c == "":
-                mode = 1 if mode == 2 else 2
-                continue
-            else:
-                c = int(c)
-
-            if 0 <= r < m.rows and 0 <= c < m.cols:
-                m.primaryClick(r, c) if mode == 1 else m.secondaryClick(r, c)
-                print(str(m).replace("0", " "))
-                if m.lose():
-                    print("YOU LOSE!")
-                    exit()
-                if m.win():
-                    print("YOU WIN!")
-                    exit()
-            else:
-                raise IndexError("Invalid index")
-        except (ValueError, IndexError) as e:
-            print(e)
-            pass
-        except KeyboardInterrupt as e:
-            print("")
-            pass
-        except EOFError as e:
-            print("Exiting...")
-            break
+        if x in F:
+            F -= {x}
+        elif x not in R:
+            F |= {x}
